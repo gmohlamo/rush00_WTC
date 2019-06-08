408d3d406e1467838d16fb30d5d40e17167686cc
Gladwin MOHLAMONYANE <gmohlamo@c4r24s2.wethinkcode.co.za>
2019-06-08 14:10:27 +0200

    Got the screen dimensions

--- view/Draw.cpp
+++ view/Draw.cpp
@@ -1,12 +1,18 @@
 #include "Draw.hpp"
 
 Draw::Draw(void) {
-	initscr();
-	cbreak();
-	noecho();
+	initscr(); //initialize terminal curses mode
+	//cbreak();
+	raw(); //See the man on this function. It prevents the tty driver from buffering typed
+	//characters until a newline or line carriage is typed.
+	noecho(); //prevent keys being entered from being echoed to the screen
+	keypad(stdscr, TRUE); //enable keyboard event capturing
+	getmaxyx(stdscr, this->_windowVert, this->_windowHor); //get thew maximum width and height values for the game.
+	this->_window = newwin(this->_windowVert, this->_windowHor, 0, 0); //allocate memory and gather information on the window.
 }
 
 Draw::~Draw(void) {
+	endwin();
 }
 
 Draw::Draw(Draw const & obj) {
@@ -15,4 +21,17 @@ Draw::Draw(Draw const & obj) {
 
 Draw	&	Draw::operator = (Draw const &){
 	return (*this);
+}
+
+void	Draw::_moveCursor(int y, int x) {
+	move(y, x);
+}
+
+void	Draw::draw(int y, int x, chtype const ch) {
+	move(y, x);
+	addch(ch);
+}
+
+void	Draw::input(void) {
+	this->_ch = getch();
 }
--- view/Draw.hpp
+++ view/Draw.hpp
@@ -9,10 +9,14 @@ class Draw {
 	Draw(Draw const & obj);
 	Draw & operator = (Draw const &);
 	~Draw(void);
+	void	draw(int y, int x, const chtype);
+	void	input(void);
 	private:
 	WINDOW*	_window;
-	int	_offsetx;
-	int	_offsety;
+	chtype	_ch;
+	int	_windowVert;
+	int	_windowHor;
+	void	_moveCursor(int y, int x);
 };
 
 #endif
